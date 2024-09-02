c26e356c33048a951e085a7e9911cd2563fb40fc
petkantc <remy.petkantchin@epfl.ch>
2024-09-02 15:50:21 +0200

    Initial commit

--- plot_flatmap_h5/plot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$1 : /gpfs/bbp.cscs.ch/project/proj82/home/petkantc/axon/axonal-projection/axon_projection/validation/circuit-build/a_s_out_lite/Morphologies/hashed/0f/0f7f991474f969ca8eaf85dcd9b68567.h5
-
-echo "INFO: Expecting a morphology file as first argument"
-python flatplot.py --dots --h5morpho axon --color '#000000' -p 512 --dual /gpfs/bbp.cscs.ch/project/proj82/home/bolanos/flatmap_both.nrrd $1 morpho_axon && \
-python flatplot.py --dots --h5morpho soma --color '#FFFFFF' --spread 2 -p 512 --dual /gpfs/bbp.cscs.ch/project/proj82/home/bolanos/flatmap_both.nrrd $1 morpho_soma && \
-convert background.png morpho_axon.png -composite temp.png && convert temp.png morpho_soma.png -composite axon_in_map.png && rm temp.png
